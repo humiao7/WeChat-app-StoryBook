eaab818468ccfe1e02792aa44b9c1061633db2cb
humiao <1124003166@qq.com>
2017-10-16 13:53:18 +0800

    添加列表布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    listInfo: ['说明文字信息1', '说明文字信息2', '说明文字信息3', '说明文字信息4', '说明文字信息5', '说明文字信息6', '说明文字信息7', '说明文字信息8']
   },
   //事件处理函数
   bindViewTap: function () {
