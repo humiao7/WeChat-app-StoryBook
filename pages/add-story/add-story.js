73ea1c1caa3f78d7be73a801147d9ac1db94a017
humiao <1124003166@qq.com>
2017-10-19 17:02:21 +0800

    添加时间戳

--- pages/add-story/add-story.js
+++ pages/add-story/add-story.js
@@ -1,66 +1,81 @@
 // pages/add-story/add-story.js
+var util = require('../../utils/util.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    edit_time: new Date()
+    new_title: "",
+    new_content: ""
+
+  },
+
+  commitStory: function () {
+    var storys = wx.getStorageSync('storys');
+    var new_story = { 'title': new_title, 'content': new_content };
+    console.log(new_story);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // 调用函数时，传入new Date()参数，返回值是日期和时间  
+    var time = util.formatTime(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据  
+    this.setData({
+      edit_time: time
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
